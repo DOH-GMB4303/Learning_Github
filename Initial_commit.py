5c44ce4fcf4b265c0f40283441f1e39675db421c
DOH-GMB4303 <DOH-GMB4303>
2022-07-08 14:38:36 +0000

    Initial

---
+++ Initial_commit.py
@@ -0,0 +1,25 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC - This notebook is used in GitHub knowledge transfer sessions.
+# MAGIC - The intent is to equip users with a solid foundation in the GitHub Ecosystem
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## GitHub
+# MAGIC Why use it?
+# MAGIC - We use GitHub because it allows fine grained version control. Version control is useful because it permits multiple users to work on a code base without affecting other user's work. 
+# MAGIC 
+# MAGIC 
+# MAGIC 
+# MAGIC Step 1. Commit this to the repo as our initial commit. (This was already done)
+# MAGIC Step 2. 
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC This is the initial commit to the repository, or Version 1. If helpful, you can imagine this text as code. 
+
+# COMMAND ----------
+
+
