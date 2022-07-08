940f5ae0a2d3ba16ff1959d82724aa88e7f3a7f5
DOH-GMB4303 <DOH-GMB4303>
2022-07-08 14:44:25 +0000

    2

--- Initial_commit.py
+++ Initial_commit.py
@@ -13,7 +13,10 @@
 # MAGIC 
 # MAGIC 
 # MAGIC Step 1. Commit this to the repo as our initial commit. (This was already done)
-# MAGIC Step 2. 
+# MAGIC 
+# MAGIC Now, imagine you would like to make a change to the code base. To do so, you first need to create a branch. Creating a branch from a repo is essentially creating a copy of the repo; we will refer to this as the "feature branch". Customarily, the production or final branch is named "main". YOU SHOULD NEVER COMMIT ANYTHING DIRECTLY TO MAIN. Rather, you should create a branch, make your changes, then push the branch to the repo. After someone approves the changes, you then merge the branch changes into the main branch. 
+# MAGIC 
+# MAGIC The benefit of using this framework is that the changes you push to a repo don't affect others' work in progress. 
 
 # COMMAND ----------
 
